dae0d42512732c107a228674b08479d6da76664d
Henrik Hofmeister <henrik@newdawn.dk>
2023-06-18 12:46:48 +0200

    fix: Change yaml-transform to TS to include

--- src/test-helpers/yaml-transform.ts
+++ src/test-helpers/yaml-transform.ts
@@ -1,6 +1,7 @@
-const YAML = require('yaml');
+import * as YAML from 'yaml';
+
 module.exports = {
-    process(sourceText, sourcePath, options) {
+    process(sourceText:string, sourcePath:string, options:any) {
         return {
             code: `module.exports = ${JSON.stringify(YAML.parse(sourceText))};`,
         };
